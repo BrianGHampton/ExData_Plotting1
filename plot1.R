803d4d929732f9481fdfc32d4bcc771cc3906b3a
Brian Hampton <78804656+BrianGHampton@users.noreply.github.com>
2021-04-27 22:07:24 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -13,7 +13,7 @@ power <- data.table::fread(input = "household_power_consumption.txt",
 power$Date <- parse_date_time(power$Date, c('dmy', 'ymd'))
 
 # Subset 'power' to just input from 2007-02-01 and 2007-02-02 (Happy Groundhog's Day!)
-power_feb07 <- power[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+power_feb07 <- power[(Date >= "2007-01-31") & (Date <= "2007-02-02")]
 
 # Create a histogram of global active power for the two days and save as a PNG 
 png("plot1.png", width=480, height=480)
