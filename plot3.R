220b468cbb5b08f18880a7a3cb2f4af65785ca1e
Brian Hampton <78804656+BrianGHampton@users.noreply.github.com>
2021-04-27 23:00:39 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -15,6 +15,10 @@ power$Date <- parse_date_time(power$Date, c('dmy', 'ymd'))
 # Subset 'power' to just input from 2007-02-01 and 2007-02-02 (Happy Groundhog's Day!)
 power_feb07 <- power[(Date >= "2007-01-31") & (Date <= "2007-02-02")]
 
+# Create a new column in 'power_feb07' that is the combined date and time
+datetime <- paste(as.Date(power_feb07$Date), power_feb07$Time)
+power_feb07$Datetime <- as.POSIXct(datetime)
+
 # Create a line plot of sub metering data versus time and save as a PNG
 png("plot3.png", width = 480, height = 480)
 with(power_feb07, {
