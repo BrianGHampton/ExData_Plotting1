032de44a09d2e4e1505739c10ffcd74fe2036abc
Brian Hampton <78804656+BrianGHampton@users.noreply.github.com>
2021-05-01 11:54:32 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,8 +2,9 @@
 ### it as a PNG) of global active power for 2007-02-01 and 2007-02-02
 ### from file: household_power_consumption.txt
 
-# Load the lubridate package
+# Load the needed packages
 library(lubridate)
+library(datasets)
 
 # Read the data (from the working directory)
 power <- data.table::fread(input = "household_power_consumption.txt",
@@ -15,8 +16,11 @@ power$Date <- parse_date_time(power$Date, c('dmy', 'ymd'))
 # Subset 'power' to just input from 2007-02-01 and 2007-02-02 (Happy Groundhog's Day!)
 power_feb07 <- power[(Date >= "2007-01-31") & (Date <= "2007-02-02")]
 
-# Create a histogram of global active power for the two days and save as a PNG 
-png("plot1.png", width=480, height=480)
-hist(power_feb07$Global_active_power, main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+# Create a histogram of global active power for the two days  
+with(power_feb07, hist(Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red"))
+
+# Save the histogram as a PNG file
+dev.copy(png, "plot1.png", width = 480, height = 480)
 dev.off()
+
